46c3cbbac0979eebeb27a1d80c22e509f2729ee9
Eli Ennab <elinaennab@gmail.com>
2022-11-24 16:45:58 +0100

    Console.log the array and added id to all objects.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 const students_images = document.querySelector('#student_image');
+
+
+
+// All students names
+const studentName = students.map(student => student.name);
+console.log("All students names:", studentName);
+
+// All students names
+const studentImage = students.map(student => student.image);
+console.log("All students images:", studentImage);
+
+// One students image
+
+
