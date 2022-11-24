2537a206270458ee5e33d3985170e7cbb81a81c1
Eli Ennab <elinaennab@gmail.com>
2022-11-24 16:58:23 +0100

    Test of map & find for all (& specific) students.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 const students_images = document.querySelector('#student_image');
 
-
+// Testing
 
 // All students names
 const studentName = students.map(student => student.name);
 console.log("All students names:", studentName);
 
-// All students names
+// All students (source of) images 
 const studentImage = students.map(student => student.image);
 console.log("All students images:", studentImage);
 
-// One students image
+// Finding a specific student
 
+// Finding a specific student whose id is '12'
+const randomStudent = students.find(obj => obj.id === 12);
 
+// Printing specific student on the console
+console.log(randomStudent);
+
+// Printing specific student image source on the console
+console.log(randomStudent.image);
