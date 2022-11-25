9a9e00ec2ac7b0071fd3842dcf490b4a28b8f64d
Eli Ennab <elinaennab@gmail.com>
2022-11-25 15:23:53 +0100

    Success in getting image into DOM.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-const students_images = document.querySelector('#student_image');
-
 // Testing
 
 // All students names
-const studentName = students.map(student => student.name);
-console.log("All students names:", studentName);
+// const studentName = students.map(student => student.name);
+// console.log("All students names:", studentName);
 
 // All students (source of) images 
 const studentImage = students.map(student => student.image);
@@ -20,3 +18,10 @@ console.log(randomStudent);
 
 // Printing specific student image source on the console
 console.log(randomStudent.image);
+const image = document.querySelector('#studentImage');
+image.src = `${randomStudent.image}`;
+
+// // Printing all images
+// const image = document.querySelector('#studentImage');
+// image.src = `${studentImage.image}`;
+
