0901aa5ba032faad90c2f681fbc004ac569bad8d
Eli Ennab <elinaennab@gmail.com>
2022-11-29 13:15:44 +0100

    A map copy of array with true and false students.

---
+++ js/script-tests.js
@@ -0,0 +1,9 @@
+// A copy of the array with true students
+const trueStudent = students.map(student => student)
+
+console.log(trueStudent);
+
+// A copy of the array with false names
+const falseStudent = students.map(student => student)
+
+console.log(falseStudent);
