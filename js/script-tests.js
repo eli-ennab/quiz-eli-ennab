1b798c9da2e600e42781d42872beec2fdb0423ed
Eli Ennab <elinaennab@gmail.com>
2022-11-29 18:21:31 +0100

    Tries on the 10 student option. TBC.

--- js/script-tests.js
+++ js/script-tests.js
@@ -1,9 +1,108 @@
+/*
+* 10 students only
+*/
+
+const gameOptions = document.querySelector('.game-options');
+let trueStudent;
+let falseStudent;
+
+// Shuffle the students
+const shuffleRandomStudent = (students) => {
+	for (let i = students.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = students[i];
+		students[i] = students[j];
+		students[j] = temp;
+	}
+}
+
+shuffleRandomStudent(students);
+
 // A copy of the array with true students
-const trueStudent = students.map(student => student)
-
+trueStudent = students.map(student => student)
 console.log(trueStudent);
+// // And slice to ten true students
+// let tenTrueStudents = (trueStudent.slice(0, 10));
+// console.log(tenTrueStudents);
+// A function that gives the new true student (name and image)
+function newTrueStudent() {
+	const randomStudentImage = trueStudent[0];   	                            // Random students name's image
+	const image = document.querySelector('#studentImage');          	        // Random student into DOM
+	image.src = `${randomStudentImage.image}`;
 
-// A copy of the array with false names
-const falseStudent = students.map(student => student)
+	const randomCorrectStudentName = trueStudent.find(student => student);	    // Random true students name
+	trueStudent = document.querySelector('#option1');				            // And into DOM
+	trueStudent.textContent = `${randomCorrectStudentName.name}`;
+};
 
-console.log(falseStudent);
+// // A copy of the array with false names
+// falseStudent = students.map(student => student)
+// console.log(falseStudent);
+
+function newFalseStudent() {
+    // WHAT TO DO!
+};
+
+gameOptions.addEventListener('click', e => {
+	e.preventDefault();
+	// console.log('You clicked the options.', e.target);
+
+	if (e.target !== trueStudent) {
+        infoBox.innerHTML = `<h4>FALSE<h4>`;
+	} else {
+        infoBox.innerHTML = `<h4>TRUE<h4>`;
+    }
+})
+
+const startNewGame10 = () => {
+	// Shuffle all the students
+	shuffleRandomStudent(students);
+
+    // Getting true student
+    newTrueStudent();
+
+    // Getting false students
+    newFalseStudent();
+}
+
+btn10.addEventListener('click', e => {
+    startNewGame10();
+});
+
+/*
+* 20 students only
+*/
+
+const startNewGame20 = () => {
+	// Shuffle all the students
+	shuffleRandomStudent(students);
+
+    // Getting true student
+    newTrueStudent();
+
+    // Getting false students
+    newFalseStudents();
+}
+
+btn20.addEventListener('click', e => {
+    startNewGame20();
+});
+
+/*
+* ALL students
+*/
+
+const startNewGameAll = () => {
+	// Shuffle all the students
+	shuffleRandomStudent(students);
+
+    // Getting true student
+    newTrueStudent();
+
+    // Getting false students
+    newFalseStudents();
+}
+
+btnAll.addEventListener('click', e => {
+    startNewGameAll();
+});
